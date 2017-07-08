e60acda679f26958644cec7b8839e2f3c6c55a21
xxkyzerxx <kyzer@MacBook-Pro.local>
2017-07-08 15:21:14 +0800

    linking data to ui

--- app/src/main/java/com/example/kyzer/psidemo/MainActivity.java
+++ app/src/main/java/com/example/kyzer/psidemo/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.pm.ActivityInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.widget.TextView;
 
 import com.example.kyzer.psidemo.Class.PSI;
 import com.example.kyzer.psidemo.Helper.PSIHelper;
@@ -44,7 +46,28 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String s) {
+            if (TextUtils.isEmpty(s)){
+                //success
+                TextView textview = (TextView) findViewById(R.id.lblCentral);
+                int temp = (int)psiData.getPSIItems().get(0).getReadings().getPsi_twenty_four_hourly().getCentral();
+                textview.setText(Integer.toString(temp));
 
+                textview = (TextView) findViewById(R.id.lblNorth);
+                temp = (int)psiData.getPSIItems().get(0).getReadings().getPsi_twenty_four_hourly().getNorth();
+                textview.setText(Integer.toString(temp));
+
+                textview = (TextView) findViewById(R.id.lblSouth);
+                temp = (int)psiData.getPSIItems().get(0).getReadings().getPsi_twenty_four_hourly().getSouth();
+                textview.setText(Integer.toString(temp));
+
+                textview = (TextView) findViewById(R.id.lblEast);
+                temp = (int)psiData.getPSIItems().get(0).getReadings().getPsi_twenty_four_hourly().getEast();
+                textview.setText(Integer.toString(temp));
+
+                textview = (TextView) findViewById(R.id.lblWest);
+                temp = (int)psiData.getPSIItems().get(0).getReadings().getPsi_twenty_four_hourly().getWest();
+                textview.setText(Integer.toString(temp));
+            }
             dialog.dismiss();
         }
 
